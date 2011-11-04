9e2d426f8446c38a535e1819798f120f6949ed7a
JJ Merelo <jj@merelo.net>
2011-11-04 14:51:45 +0100

    Varios bucles

---
+++ js/design-docs-load.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+curl -X PUT http://127.0.0.1:5984/sofea_test/_design/docs -H "Content-Type: application/json" -d @docs_count.json
+curl -X PUT http://127.0.0.1:5984/sofea_test/_design/rev -H "Content-Type: application/json" -d @revs.json
---
+++ perl/evaluate-pop-loop.pl
@@ -0,0 +1,55 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use lib qw(/home/jmerelo/progs/SimplEA/trunk/Algorithm-Evolutionary-Simple/lib );
+
+use YAML qw(LoadFile Dump); 
+use CouchDB::Client;
+use Algorithm::Evolutionary::Simple qw(random_chromosome max_ones);
+use JSON qw(encode_json);
+use LWP::UserAgent;
+
+my $conf = LoadFile('conf.yaml') || die "No puedo cargar la configuracion : $!\n";
+my $c = CouchDB::Client->new(uri => $conf->{'couchurl'});
+$c->testConnection or die "The server cannot be reached";
+print "Running version " . $c->serverInfo->{version} . "\n";
+my $db;
+eval {
+  $db = $c->newDB($conf->{'couchdb'})->create;
+};
+if ( $@ ) {
+  $db = $c->newDB($conf->{'couchdb'});
+}
+print "Connected to $conf->{'couchdb'}\n";
+
+my $population_size = shift || 32;
+
+my $doc = $db->newDesignDoc('_design/rev')->retrieve;
+my $view = $doc->queryView( "rev1", startkey => 0,
+			      limit=> $population_size );
+
+while (  @{$view->{'rows'}} ) {
+ 
+  my @updated_docs;
+  for my $p ( @{$view->{'rows'}} ) {
+    push @updated_docs, 
+      $db->newDoc( $p->{'value'}{'_id'},
+		   $p->{'value'}{'_rev'}, 
+		   { fitness => max_ones( $p->{'value'}{'str'})} );
+  }
+  my $response = $db->bulkStore( \@updated_docs );
+  
+  print encode_json( $response );
+  
+  $view = $doc->queryView( "rev1", startkey => 0,
+			   limit=> $population_size );
+
+} 
+
+
+
+
+#-----------------------------
+
